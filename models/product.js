06452937a7e68094aac9b0df6714d9162609c746
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-22 16:48:59 +0200

    CREATE product Model

---
+++ models/product.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String, 
+    countInStock: { type: Number, required, true},
+})
+
+Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
