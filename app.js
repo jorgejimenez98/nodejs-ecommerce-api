726b09585292fee99006a6d0e3463611993e41a0
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-22 16:06:48 +0200

    ADD body-parser to express app to read data

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 require("dotenv/config");
 
 const port = process.env.PORT || 5000;
