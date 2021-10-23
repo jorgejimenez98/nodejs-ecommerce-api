a0de330cb4fc313df7a4390715ad228cf02b06da
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-23 10:45:40 +0200

    REFORMAT app file

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
 const express = require("express");
 const app = express();
 
-const api = process.env.API_URL;
-
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-
-// Middlerware
-app.use(bodyParser.json()); // Read data from frontend
-app.use(morgan("tiny")); // Log API method details
-
+// Dependencies
 require("dotenv/config");
 require("./db");
 
-// Routes Imports
-const productRoutes = require("./routers/productRoutes");
-// ADD routes to APP
-app.use(`${api}/products`, productRoutes);
+//Routes Imports
+const productsRoutes = require("./routes/productRoutes");
+
+// Middlerware Settings
+app.use(require("morgan")("tiny")); // Log API method details
+app.use(express.json()); // Read data from frontend
+app.use(express.text());
+
+// ADD ROUTES TO APP
+app.use("/api/products", productsRoutes);
 
 const port = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("API working");
-});
-
-app.listen(port, () => {
-  console.log(`The server is running on http://localhost:${port}`);
-});
+app.get("/", (req, res) => res.send("APP working"));
+app.listen(port, () => console.log(`Server on http://localhost:${port}`));
