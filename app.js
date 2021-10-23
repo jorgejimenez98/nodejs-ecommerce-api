7d32500b07551a7fab6168f55b4e045b2543e8de
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-23 10:53:39 +0200

    ADD user, category, order routes to app

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ require("./db");
 
 //Routes Imports
 const productsRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 
 // Middlerware Settings
 app.use(require("morgan")("tiny")); // Log API method details
@@ -14,7 +17,10 @@ app.use(express.json()); // Read data from frontend
 app.use(express.text());
 
 // ADD ROUTES TO APP
+app.use("/api/users", userRoutes);
 app.use("/api/products", productsRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/orders", orderRoutes);
 
 const port = process.env.PORT || 5000;
 app.get("/", (req, res) => res.send("APP working"));
