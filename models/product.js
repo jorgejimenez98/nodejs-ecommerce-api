9f29159f78015b53131d39e11f22735c073a30a1
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-23 11:14:06 +0200

    UPDATE product model atributes

--- models/product.js
+++ models/product.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-  name: String,
-  image: String,
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  description: { type: String, required: true },
+  richDescription: { type: String, default: "" },
+  image: { type: String, default: "" },
+  images: [{ type: String }],
+  brand: { type: String, default: "" },
+  price: { type: Number, required: true },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
   countInStock: { type: Number, required: true },
+  rating: { type: Number, required: true, min: 0, max: 255 },
+  isFeatured: { type: Boolean, default: true },
+  dateCreated: { type: Date, default: Date.now },
 });
 
 Product = mongoose.model("Product", productSchema);
