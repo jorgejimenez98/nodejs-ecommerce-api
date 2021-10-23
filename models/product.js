ab3552adf43be7df2d5ab3965f5c59f27fb49c1c
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-23 09:20:39 +0200

    FIX sintax error on countInStock atribute

--- models/product.js
+++ models/product.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String, 
-    countInStock: { type: Number, required, true},
-})
+  name: String,
+  image: String,
+  countInStock: { type: Number, required: true },
+});
 
 Product = mongoose.model("Product", productSchema);
 
