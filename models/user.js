97d281a0ce5cb1f994887bf13d096b554df83228
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-24 15:22:26 +0200

    UPDATE user atributes

--- models/user.js
+++ models/user.js
@@ -1,14 +1,56 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  street: {
+    type: String,
+    default: "",
+  },
+  apartment: {
+    type: String,
+    default: "",
+  },
+  zip: {
+    type: String,
+    default: "",
+  },
+  city: {
+    type: String,
+    default: "",
+  },
+  country: {
+    type: String,
+    default: "",
   },
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
 
+userSchema.set("toJSON", {
+  virtuals: true,
+});
+
+const User = mongoose.model("User", userSchema);
+exports.userSchema = userSchema;
 module.exports = User;
