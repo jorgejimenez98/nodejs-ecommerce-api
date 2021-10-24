a46e7fb558bb3f8dd3f74ed16db09c3a88c0ad7b
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-24 10:36:59 +0200

    ADD status to response on category list

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get(`/`, async (req, res) => {
   const categoryList = await Category.find();
   if (!categoryList) res.status(500).json({ success: false });
-  res.send(categoryList);
+  res.status(200).send(categoryList);
 });
 
 module.exports = router;
