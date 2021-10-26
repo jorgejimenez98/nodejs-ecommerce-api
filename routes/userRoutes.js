15b970f10f3572619793066481e47fdd9a07a24c
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-26 15:26:48 +0200

    ADD register user method to user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -107,4 +107,24 @@ router.delete("/:id", async (req, res) => {
     });
 });
 
+router.post("/register", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+  user = await user.save();
+
+  if (!user) return res.status(400).send("the user cannot be created!");
+
+  res.send(user);
+});
+
 module.exports = router;
