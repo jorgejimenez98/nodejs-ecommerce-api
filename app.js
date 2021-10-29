9b01c95c0ae3c14c017a9644558f011e179d3a45
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-29 13:01:08 +0200

    REDUCE routes imports

--- app.js
+++ app.js
@@ -10,12 +10,6 @@ const cors = require("cors");
 const authJWT = require("./helpers/jwt");
 const errorHandler = require("./helpers/errorhandler");
 
-//Routes Imports
-const productsRoutes = require("./routes/productRoutes");
-const userRoutes = require("./routes/userRoutes");
-const categoryRoutes = require("./routes/categoryRoutes");
-const orderRoutes = require("./routes/orderRoutes");
-
 // Middlerware Settings
 app.use(require("morgan")("tiny")); // Log API method details
 app.use(express.json()); // Read data from frontend
@@ -23,13 +17,13 @@ app.use(cors());
 app.options("*", cors());
 app.use(authJWT()); // Protect API Authentication
 app.use(errorHandler);
-app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 
 // ADD ROUTES TO APP
-app.use("/api/users", userRoutes);
-app.use("/api/products", productsRoutes);
-app.use("/api/categories", categoryRoutes);
-app.use("/api/orders", orderRoutes);
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/categories", require("./routes/categoryRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
 
 const port = process.env.PORT || 5000;
 app.get("/", (req, res) => res.send("APP working"));
